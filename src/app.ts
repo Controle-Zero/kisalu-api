9a2d35e69a8e58485561d9f4f684c2556b030045
regireis7 <regi@fisastrategy.com>
2021-12-09 02:32:37 +0100

    tsconfig updates

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import clienteRoutes from "../src/routes/cliente.routes";
-import log from "../src/log/index"
+import clienteRoutes from "./routes/cliente.routes";
+import log from "./log"
 
 dotenv.config();
 
