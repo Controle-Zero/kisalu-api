373fd238fc60955f461b2b2dc5e08edabc21935b
regireis7 <regi@fisastrategy.com>
2022-01-20 04:57:41 +0100

    Removed duplicated logic"

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,10 @@
 import app from "../apps/httpApp";
 import { log } from "../log";
 import http from "http";
+import dotenv from "dotenv";
 import webSocketApp from "../apps/webSocketApp";
+
+dotenv.config();
 
 const httpServer = http.createServer(app);
 webSocketApp(httpServer);
