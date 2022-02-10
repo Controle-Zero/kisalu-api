431ba33584debfa17ee669213dc5281e4f7d9991
regireis7 <regi@fisastrategy.com>
2022-02-10 15:36:29 +0100

    Changed from FACTURA to DOCUMENTO

--- src/libs/document_generator/templates/atividade.templates.ts
+++ src/libs/document_generator/templates/atividade.templates.ts
@@ -20,7 +20,7 @@ export function atividadeTemplateFile() {
               <div class="invoice-wrapper">
                 <div class="intro">
                   <h2 id="company-name">UNION SERVICES</h2>
-                  <h2 id="fatura">FACTURA</h2>
+                  <h2 id="fatura">DOCUMENTO</h2>
                   <strong>{{atividade.id}}</strong>
                 </div>
   
