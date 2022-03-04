72ce5e489035204319e5d99d0137b48c38125967
regireis7 <regi@fisastrategy.com>
2022-03-04 03:24:16 +0100

    change estado

--- src/services/prestador/atividadesHandler.ts
+++ src/services/prestador/atividadesHandler.ts
@@ -8,7 +8,7 @@ export async function getAtividadesCompletas(id: string) {
     select: {
       atividades: {
         where: {
-          estado: "COMPLETA",
+          estado: "FINALIZADA",
         },
       },
     },
