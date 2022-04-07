aee0f5916530f4e19531860668576a434086b3bb
regireis7 <regi@fisastrategy.com>
2022-04-07 21:52:46 +0100

    Added Multiple Clusters logic

--- src/channels/atividade/handler.ts
+++ src/channels/atividade/handler.ts
@@ -14,33 +14,30 @@ dotenv.config();
 export async function atividadeChannel(io: Server) {
   let sockets = [];
 
-  io.of(process.env.SOCKETS_ATIVIDADE_NSP).on(
-    "connection",
-    (socket: Socket) => {
-      log.info(`Socket ${socket.id} connected`);
+  io.of(process.env.SOCKETS_NAMESPACE).on("connection", (socket: Socket) => {
+    log.info(`Socket ${socket.id} connected`);
 
-      const { token } = socket.handshake.auth;
+    const { token } = socket.handshake.auth;
 
-      if (!token) {
-        log.info("Token hasn't been informed...");
-        socket.disconnect();
-      }
-
-      const userID = verifyToken(token);
-
-      if (userID && verifyTokenDB(token)) {
-        handleSocketsArray(userID, { socket, sockets });
-
-        socket.on(Events.REQUEST, (payload: RequestPayload) => {
-          requestEventHandler({ payload, io });
-        });
-
-        socket.on(Events.RESPONSE, (payload: ResponsePayload) => {
-          responseEventHandler({ payload, socket, sockets });
-        });
-      } else {
-        socket.disconnect();
-      }
+    if (!token) {
+      log.info("Token hasn't been informed...");
+      socket.disconnect();
     }
-  );
+
+    const userID = verifyToken(token);
+
+    if (userID && verifyTokenDB(token)) {
+      handleSocketsArray(userID, { socket, sockets });
+
+      socket.on(Events.REQUEST, (payload: RequestPayload) => {
+        requestEventHandler({ payload, io });
+      });
+
+      socket.on(Events.RESPONSE, (payload: ResponsePayload) => {
+        responseEventHandler({ payload, socket, sockets });
+      });
+    } else {
+      socket.disconnect();
+    }
+  });
 }
--- src/server/index.ts
+++ src/server/index.ts
@@ -3,12 +3,45 @@ import { log } from "../libs/log";
 import http from "http";
 import dotenv from "dotenv";
 import webSocketApp from "../apps/webSocketApp";
+import cluster from "cluster";
+import { cpus } from "os";
+import { setupMaster } from "@socket.io/sticky";
+import { setupPrimary } from "@socket.io/cluster-adapter";
 
 dotenv.config();
 
-const httpServer = http.createServer(app);
-webSocketApp(httpServer);
+const numCPUs = cpus.length;
 
-httpServer.listen(process.env.PORT || 8080, () => {
-  log.info(`App started running on port ${process.env.PORT ? process.env.PORT : 8080}`);
-});
+if (cluster.isPrimary) {
+  log.info(
+    `Server (Primary) ${process.pid} is running on port ${process.env.PORT}`
+  );
+
+  const primaryHttpServer = http.createServer(app);
+
+  setupMaster(primaryHttpServer, {
+    loadBalancingMethod: "least-connection",
+  });
+
+  setupPrimary();
+
+  cluster.setupPrimary({
+    args: ["--use", "http"],
+  });
+
+  primaryHttpServer.listen(process.env.PORT || 8080);
+
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker) => {
+    log.info(`Worker ${worker.process.pid} died`);
+    cluster.fork();
+  });
+} else {
+  log.info(`Server (Worker) ${process.pid} is running`);
+
+  const workerHttpServer = http.createServer(app);
+  webSocketApp(workerHttpServer);
+}
