e80878c508a8e5361fc1f14cbc26e3c938d3f13b
regireis7 <regi@fisastrategy.com>
2022-04-12 05:20:24 +0100

    Set Up Swagger Structure

--- src/apps/httpApp.ts
+++ src/apps/httpApp.ts
@@ -5,12 +5,15 @@ import { prestadorRoutes } from "../routes/prestador.routes";
 import { categoriaRoutes } from "../routes/categoria.routes";
 import cors from "cors";
 import { atividadeRoutes } from "../routes/atividade.routes";
+import swaggerUI from "swagger-ui-express";
+import swaggerConfing from "../libs/configs/swagger";
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
+app.use("/api/doc", swaggerUI.serve, swaggerUI.setup(swaggerConfing));
 app.use("/cliente", clienteRoutes);
 app.use("/prestador", prestadorRoutes);
 app.use("/categoria", categoriaRoutes);
