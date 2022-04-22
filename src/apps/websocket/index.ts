620b73788ea8d0420dc085fd898654fce7b938ec
regireis7 <regi@fisastrategy.com>
2022-04-22 03:45:55 +0100

    Removed Multi-Socket Clusters logic

--- src/apps/websocket/index.ts
+++ src/apps/websocket/index.ts
@@ -1,32 +1,8 @@
 import { Server } from "socket.io";
 import { mainChannel } from "../../channels/handler";
 import http from "http";
-import { createAdapter as createClusterAdapter } from "../../../node_modules/@socket.io/cluster-adapter";
-import { createAdapter as createRedisAdapter } from "../../../node_modules/@socket.io/redis-adapter";
-import { setupWorker } from "@socket.io/sticky";
-import redisConfig from "../../libs/configs/redis";
 
 export default function webSocketApp(httpServer: http.Server) {
-  const io = new Server(httpServer, {
-    cors: {
-      origin: "*",
-    },
-    path: "/websocket",
-  });
-
-  const pubClient = redisConfig;
-  const subClient = pubClient.duplicate();
-
-  io.adapter(createRedisAdapter(pubClient, subClient));
-
-  io.adapter(createClusterAdapter());
-
-  setupWorker(io);
-
-  mainChannel(io);
-}
-
-export function singleServerWebSocket(httpServer: http.Server) {
   const io = new Server(httpServer, {
     cors: {
       origin: "*",
